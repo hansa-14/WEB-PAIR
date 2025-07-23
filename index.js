6341ecd9eed2f4cd4d7986100c79994ce0b1d7a2
hansa-14 <hansadewmina14@gmail.com>
2025-07-23 23:36:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+__path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/qr', server);
-app.use('/code', code);
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/index.html')
-})
+
+// Apply body parsers BEFORE routes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:` + PORT)
-})
 
-module.exports = app
+// Import routes AFTER parsers
+let server = require('./qr');
+let code = require('./pair');
+
+app.use('/qr', server);
+app.use('/code', code);
+
+app.use('/', async (req, res, next) => {
+  res.sendFile(__path + '/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:` + PORT);
+});
+
+module.exports = app;

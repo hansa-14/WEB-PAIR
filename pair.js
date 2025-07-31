f18183e41c98ff5603ccfaf221227f8cbf7e24c8
hansa-14 <hansadewmina14@gmail.com>
2025-08-01 01:04:26 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -16,6 +16,10 @@ const {
 const { upload } = require("./mega");
 
 const MESSAGE = process.env.MESSAGE || `✅ SESSION GENERATED SUCCESSFULLY`;
+const fs = require("fs");
+const path = require("path");
+const authFolder = path.join(__dirname, "auth_info_baileys");
+if (!fs.existsSync(authFolder)) fs.mkdirSync(authFolder, { recursive: true });
 
 function removeFile(FilePath) {
   if (fs.existsSync(FilePath)) {
